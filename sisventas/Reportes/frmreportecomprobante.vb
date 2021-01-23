dd4be9c13a0cda732e8a753991f96f5ae5bd5a1f
Mauro Sanchez <mjscontacto@gmail.com>
2021-01-23 14:41:20 -0300

    Primer commit

---
+++ sisventas/Reportes/frmreportecomprobante.vb
@@ -0,0 +1,8 @@
+﻿Public Class frmreportecomprobante
+
+    Private Sub frmreportecomprobante_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: esta línea de código carga datos en la tabla 'dbventasDataSet1.generar_comprobante' Puede moverla o quitarla según sea necesario.
+        Me.generar_comprobanteTableAdapter.Fill(Me.dbventasDataSet1.generar_comprobante, idventa:=txtidventa.Text)
+        Me.ReportViewer1.RefreshReport()
+    End Sub
+End Class
